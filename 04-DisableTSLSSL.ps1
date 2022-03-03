d730ec271e658eb2a910a6f4e4c10ebeb213ccc6
Trevor Saudi <38811409+trevorsaudi@users.noreply.github.com>
2022-03-03 11:51:05 +0300

    Create 04-DisableTSLSSL.ps1

---
+++ 04-DisableTSLSSL.ps1
@@ -0,0 +1,2 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
